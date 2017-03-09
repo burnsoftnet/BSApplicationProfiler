363bb414bded16da2fdfe38295151d167bc5c846
Joe Mireles <bs.GitHub@burnsoft.net>
2017-03-09 15:40:55 -0500

    Updated Session Details Page

--- BSAP_UI_WebForms/BSAP_UI_WebForms/App_Code/BSDatabase.vb
+++ BSAP_UI_WebForms/BSAP_UI_WebForms/App_Code/BSDatabase.vb
@@ -29,7 +29,7 @@ Namespace BurnSoft
         Public Function GetProjectName(APNID As Long, Optional ByRef errorID As Long = 0, Optional errMsg As String = "") As String
             Dim sAns As String = ""
             Try
-                If ConnectDB() Then
+                If ConnectDB(errorID, errMsg) Then
                     Dim SQL As String = "select * from app_project_name where ID=" & APNID
                     Dim CMD As New MySqlCommand(SQL, conn)
                     Dim RS As MySqlDataReader
@@ -50,5 +50,60 @@ Namespace BurnSoft
             End Try
             Return sAns
         End Function
+        Public Sub GetSessionTimes(SessionID As Long, ByRef SessionStart As String, SessionEnd As String, Optional ByRef errorID As Long = 0, Optional errMsg As String = "")
+            Try
+                If ConnectDB(errorID, errMsg) Then
+                    Dim SQL As String = "select * from monitoring_session where id=" & SessionID
+                    Dim CMD As New MySqlCommand(SQL, conn)
+                    Dim RS As MySqlDataReader
+                    RS = CMD.ExecuteReader
+                    While RS.Read
+                        If Not IsDBNull(RS("sessiondt")) Then SessionStart = RS("sessiondt")
+                        If Not IsDBNull(RS("sessionend")) Then
+                            SessionEnd = RS("sessionend")
+                        Else
+                            SessionEnd = "In Progress"
+                        End If
+                    End While
+                    RS.Close()
+                    RS = Nothing
+                    CMD = Nothing
+                    Call CloseDB()
+                Else
+                    SessionStart = "N/A"
+                    SessionEnd = "N/A"
+                End If
+            Catch ex As Exception
+                errorID = Err.Number
+                errMsg = ex.Message.ToString
+            End Try
+        End Sub
+        Private Function GetDistinctProcessDetails(SessionID As Long, sColumn As String, Optional ByRef errorID As Long = 0, Optional errMsg As String = "") As String
+            Dim sAns As String = "N/A"
+            Try
+                If ConnectDB(errorID, errMsg) Then
+                    Dim SQL As String = "select distinct(" & sColumn & ") as myvalue from process_stats_main where sessionid=" & SessionID
+                    Dim CMD As New MySqlCommand(SQL, conn)
+                    Dim RS As MySqlDataReader
+                    RS = CMD.ExecuteReader
+                    While RS.Read
+                        If Not IsDBNull(RS("myvalue")) Then sAns = RS("myvalue")
+                    End While
+                    RS.Close()
+                    RS = Nothing
+                    CMD = Nothing
+                    Call CloseDB()
+                End If
+            Catch ex As Exception
+                errorID = Err.Number
+                errMsg = ex.Message.ToString
+            End Try
+            Return sAns
+        End Function
+        Public Sub GetProcessDEtails(SessionID As Long, ByRef imagename As String, username As String, commandline As String, Optional ByRef errorID As Long = 0, Optional errMsg As String = "")
+            imagename = GetDistinctProcessDetails(SessionID, "imagename", errorID, errMsg)
+            username = GetDistinctProcessDetails(SessionID, "username", errorID, errMsg)
+            commandline = GetDistinctProcessDetails(SessionID, "commandline", errorID, errMsg)
+        End Sub
     End Class
 End Namespace
---
+++ BSAP_UI_WebForms/BSAP_UI_WebForms/controls/Session_Details.ascx.vb
@@ -0,0 +1,18 @@
+﻿Public Class Session_Details1
+    Inherits System.Web.UI.UserControl
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim SessionID As Long = CLng(Request.QueryString("SessionID"))
+        Dim SQL As String = "select p.*,TIME_FORMAT(p.dt,'%H:%I') as dt_time from process_stats_main p where p.SessionID=" & SessionID
+        Chart1.Titles.Add("CPU")
+        Chart2.Titles.Add("MEMORY")
+        Chart3.Titles.Add("Handles")
+        Chart4.Titles.Add("Threads")
+        SqlDataSource1.SelectCommand = SQL
+        SqlDataSource2.SelectCommand = "select * from logs_main where sessionid=" & SessionID
+        Dim Obj As New BurnSoft.BSDatabase
+        Obj.GetProcessDEtails(SessionID, lblProcessName.Text, lblUser.Text, lblCommandLine.Text)
+        Obj.GetSessionTimes(SessionID, lblStart.Text, lblEnd.Text)
+    End Sub
+
+End Class
