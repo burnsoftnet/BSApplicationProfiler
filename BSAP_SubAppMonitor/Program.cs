cf276babc65275345943a1813e601e650842defb
JoeM <joem@BURNSOFTAP02>
2017-09-17 17:39:01 -0400

    Adding SubProcess/Child Process monitor

---
+++ BSAP_SubAppMonitor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using BurnSoft.Universal;
+namespace BSAP_SubAppMonitor
+{
+    class Program
+    {
+        private static int _PID;
+        private void init()
+        {
+            bool didexist = false;
+            BSOtherObjects obj = new BSOtherObjects();
+            _PID = Convert.ToInt32(obj.GetCommand("PID", 0,ref didexist));
+
+
+        }
+        public static void listChildProcesses(int parentProcessId)
+        {
+            String myQuery = string.Format("select * from win32_process where ParentProcessId={0}", parentProcessId);
+            ObjectQuery objQuery = new ObjectQuery(myQuery);
+            ManagementObjectSearcher objSearcher = new ManagementObjectSearcher(objQuery);
+            ManagementObjectCollection processList = objSearcher.Get();
+
+            foreach (ManagementObject item in processList)
+            {
+                try
+                {
+                    int processId = Convert.ToInt32(item["ProcessId"].ToString());
+
+                    Console.WriteLine("processId:{0} name:{1} {2}",
+                        item["ProcessId"],
+                        item["Name"],
+                        item["ParentProcessId"]
+                    );
+                    listChildProcesses(Convert.ToInt32(item["ProcessId"]));
+
+                    // recursive call
+                    if (processId != parentProcessId)
+                        listChildProcesses(processId);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
