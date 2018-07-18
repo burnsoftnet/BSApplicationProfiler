4ab0c84be676eafb85fe64307792ae541ec102aa
Joe Mireles <joe.mireles@atlasied.com>
2018-07-18 09:27:21 -0400

    #19 Renamed Variables to standard

--- BSAP_Config/Form1.vb
+++ BSAP_Config/Form1.vb
@@ -1,9 +1,9 @@
 ﻿Imports System.Configuration
 Public Class Form1
-    Const APP_PROFILER = "\BSApplicationProfiler.exe"
-    Const APP_MONITOR = "\BSAP_AppMonitor.exe"
-    Const APP_DATADUMP = "\BSAP_DataDump.exe"
-    Const APP_SERVICE = "\BSAP_Service.exe"
+    Const AppProfiler = "\BSApplicationProfiler.exe"
+    Const AppMonitor = "\BSAP_AppMonitor.exe"
+    Const AppDatadump = "\BSAP_DataDump.exe"
+    Const AppService = "\BSAP_Service.exe"
     ''' <summary>
     ''' Load Values into the fields when the form starts up
     ''' </summary>
@@ -19,97 +19,97 @@ Public Class Form1
     ''' Sub to Save values to the app.config file based on the sPath ( path of the config file) 
     ''' and the new value
     ''' </summary>
-    ''' <param name="SPath"></param>
+    ''' <param name="sPath"></param>
     ''' <param name="sKey"></param>
     ''' <param name="sValue"></param>
-    Public Sub ChangeAppSettings(ByVal SPath As String, ByVal sKey As String, ByVal sValue As String)
-        Dim Config As Configuration = ConfigurationManager.OpenExeConfiguration(SPath)
-        Config.AppSettings.Settings.Remove(sKey)
-        Config.AppSettings.Settings.Add(sKey, sValue)
-        Config.Save(ConfigurationSaveMode.Modified)
+    Public Sub ChangeAppSettings(ByVal sPath As String, ByVal sKey As String, ByVal sValue As String)
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(sPath)
+        config.AppSettings.Settings.Remove(sKey)
+        config.AppSettings.Settings.Add(sKey, sValue)
+        config.Save(ConfigurationSaveMode.Modified)
         ConfigurationManager.RefreshSection("appSettings")
     End Sub
     sub LoadServiceValues()
         On Error Resume Next
-        Dim AppPath As String = Application.StartupPath & APP_SERVICE
-        Dim Config As Configuration = ConfigurationManager.OpenExeConfiguration(AppPath)
-        chkOffLine.Checked = CBool(Config.AppSettings.Settings.Item("RUN_OFFLINE").Value)
+        Dim appPath As String = Application.StartupPath & AppService
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(appPath)
+        chkOffLine.Checked = CBool(config.AppSettings.Settings.Item("RUN_OFFLINE").Value)
     End sub
     ''' <summary>
     ''' Load the settings from the Data Dumper config file
     ''' </summary>
     Sub LoadDataDumperValues()
         On Error Resume Next
-        Dim AppPath As String = Application.StartupPath & APP_DATADUMP
-        Dim Config As Configuration = ConfigurationManager.OpenExeConfiguration(AppPath)
-        chkDebugDD.Checked = CBool(Config.AppSettings.Settings.Item("DEBUG").Value)
-        chkConsoleDD.Checked = CBool(Config.AppSettings.Settings.Item("CONSOLE").Value)
-        chkLogFileDD.Checked = CBool(Config.AppSettings.Settings.Item("USE_LOGFILE").Value)
-        chkNTEventDD.Checked = CBool(Config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
-        txtLogFileDD.Text = Config.AppSettings.Settings.Item("LOGFILE").Value
-        txtDebugDD.Text = Config.AppSettings.Settings.Item("BUGFILE").Value
-        txtEventSourceDD.Text = Config.AppSettings.Settings.Item("EVENT_SOURCE").Value
-        nudEventWarningDD.Value = Config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
-        nudEventInfoDD.Value = Config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
-        nudEventErrorDD.Value = Config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
-        cmbBugModeDD.Text = Config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
+        Dim appPath As String = Application.StartupPath & AppDatadump
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(appPath)
+        chkDebugDD.Checked = CBool(config.AppSettings.Settings.Item("DEBUG").Value)
+        chkConsoleDD.Checked = CBool(config.AppSettings.Settings.Item("CONSOLE").Value)
+        chkLogFileDD.Checked = CBool(config.AppSettings.Settings.Item("USE_LOGFILE").Value)
+        chkNTEventDD.Checked = CBool(config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
+        txtLogFileDD.Text = config.AppSettings.Settings.Item("LOGFILE").Value
+        txtDebugDD.Text = config.AppSettings.Settings.Item("BUGFILE").Value
+        txtEventSourceDD.Text = config.AppSettings.Settings.Item("EVENT_SOURCE").Value
+        nudEventWarningDD.Value = config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
+        nudEventInfoDD.Value = config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
+        nudEventErrorDD.Value = config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
+        cmbBugModeDD.Text = config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
     End Sub
     ''' <summary>
     ''' Load the settings from the Application Monitor config file
     ''' </summary>
     Sub LoadAppMonitorValues()
         On Error Resume Next
-        Dim AppPath As String = Application.StartupPath & APP_MONITOR
-        Dim Config As Configuration = ConfigurationManager.OpenExeConfiguration(AppPath)
-        chkDebugAM.Checked = CBool(Config.AppSettings.Settings.Item("DEBUG").Value)
-        chkConsoleAM.Checked = CBool(Config.AppSettings.Settings.Item("CONSOLE").Value)
-        chkLogFileAM.Checked = CBool(Config.AppSettings.Settings.Item("USE_LOGFILE").Value)
-        chkNTEventAM.Checked = CBool(Config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
-        txtLogFileAM.Text = Config.AppSettings.Settings.Item("LOGFILE").Value
-        txtDebugAM.Text = Config.AppSettings.Settings.Item("BUGFILE").Value
-        nudTimerAM.Value = Config.AppSettings.Settings.Item("TIMER_INTERVAL").Value
-        txtEventSourceAM.Text = Config.AppSettings.Settings.Item("EVENT_SOURCE").Value
-        nudEventWarningAM.Value = Config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
-        nudEventInfoAM.Value = Config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
-        nudEventErrorAM.Value = Config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
-        cmbBugModeAM.Text = Config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
+        Dim appPath As String = Application.StartupPath & AppMonitor
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(appPath)
+        chkDebugAM.Checked = CBool(config.AppSettings.Settings.Item("DEBUG").Value)
+        chkConsoleAM.Checked = CBool(config.AppSettings.Settings.Item("CONSOLE").Value)
+        chkLogFileAM.Checked = CBool(config.AppSettings.Settings.Item("USE_LOGFILE").Value)
+        chkNTEventAM.Checked = CBool(config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
+        txtLogFileAM.Text = config.AppSettings.Settings.Item("LOGFILE").Value
+        txtDebugAM.Text = config.AppSettings.Settings.Item("BUGFILE").Value
+        nudTimerAM.Value = config.AppSettings.Settings.Item("TIMER_INTERVAL").Value
+        txtEventSourceAM.Text = config.AppSettings.Settings.Item("EVENT_SOURCE").Value
+        nudEventWarningAM.Value = config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
+        nudEventInfoAM.Value = config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
+        nudEventErrorAM.Value = config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
+        cmbBugModeAM.Text = config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
     End Sub
     ''' <summary>
     ''' Load the values from the Application Profiler app.config file
     ''' </summary>
     Sub LoadProfilerValues()
         On Error Resume Next
-        Dim AppPath As String = Application.StartupPath & APP_PROFILER
-        Dim Config As Configuration = ConfigurationManager.OpenExeConfiguration(AppPath)
-        chkDebug.Checked = CBool(Config.AppSettings.Settings.Item("DEBUG").Value)
-        chkConsole.Checked = CBool(Config.AppSettings.Settings.Item("CONSOLE").Value)
-        chkLogFile.Checked = CBool(Config.AppSettings.Settings.Item("USE_LOGFILE").Value)
-        chkNTEvent.Checked = CBool(Config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
-        txtLogFile.Text = Config.AppSettings.Settings.Item("LOGFILE").Value
-        txtDebug.Text = Config.AppSettings.Settings.Item("BUGFILE").Value
-        nudTimer.Value = Config.AppSettings.Settings.Item("TIMER_INTERVAL").Value
-        txtEventSource.Text = Config.AppSettings.Settings.Item("EVENT_SOURCE").Value
-        nudEventWarning.Value = Config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
-        nudEventInfo.Value = Config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
-        nudEventError.Value = Config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
-        txtDBHost.Text = Config.AppSettings.Settings.Item("DB_HOST").Value
-        txtDBNAME.Text = Config.AppSettings.Settings.Item("DB_NAME").Value
-        txtPWD.Text = Config.AppSettings.Settings.Item("DB_PWD").Value
-        txtUID.Text = Config.AppSettings.Settings.Item("DB_UID").Value
-        nudHeartBeat.Value = Config.AppSettings.Settings.Item("HEARTBEAT_INTERVAL").Value
-        nudDBResfreh.Value = Config.AppSettings.Settings.Item("DBREFRESH_INTERVAL").Value
-        cmbBugMode.Text = Config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
+        Dim appPath As String = Application.StartupPath & AppProfiler
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(appPath)
+        chkDebug.Checked = CBool(config.AppSettings.Settings.Item("DEBUG").Value)
+        chkConsole.Checked = CBool(config.AppSettings.Settings.Item("CONSOLE").Value)
+        chkLogFile.Checked = CBool(config.AppSettings.Settings.Item("USE_LOGFILE").Value)
+        chkNTEvent.Checked = CBool(config.AppSettings.Settings.Item("USE_EVENT_LOG").Value)
+        txtLogFile.Text = config.AppSettings.Settings.Item("LOGFILE").Value
+        txtDebug.Text = config.AppSettings.Settings.Item("BUGFILE").Value
+        nudTimer.Value = config.AppSettings.Settings.Item("TIMER_INTERVAL").Value
+        txtEventSource.Text = config.AppSettings.Settings.Item("EVENT_SOURCE").Value
+        nudEventWarning.Value = config.AppSettings.Settings.Item("EVENT_ID_WARN").Value
+        nudEventInfo.Value = config.AppSettings.Settings.Item("EVENT_ID_INFO").Value
+        nudEventError.Value = config.AppSettings.Settings.Item("EVENT_ID_ERROR").Value
+        txtDBHost.Text = config.AppSettings.Settings.Item("DB_HOST").Value
+        txtDBNAME.Text = config.AppSettings.Settings.Item("DB_NAME").Value
+        txtPWD.Text = config.AppSettings.Settings.Item("DB_PWD").Value
+        txtUID.Text = config.AppSettings.Settings.Item("DB_UID").Value
+        nudHeartBeat.Value = config.AppSettings.Settings.Item("HEARTBEAT_INTERVAL").Value
+        nudDBResfreh.Value = config.AppSettings.Settings.Item("DBREFRESH_INTERVAL").Value
+        cmbBugMode.Text = config.AppSettings.Settings.Item("BUGFILE_LEVEL").Value
     End Sub
 
     Sub SaveServiceSettings()
-        Dim sPath As String = Application.StartupPath & APP_SERVICE
+        Dim sPath As String = Application.StartupPath & AppService
         Call ChangeAppSettings(sPath, "RUN_OFFLINE", chkOffline.Checked)
     End Sub
     ''' <summary>
     ''' Save the Changes to the App Monitor config file
     ''' </summary>
     Sub SaveAppMonitorSettings()
-        Dim sPath As String = Application.StartupPath & APP_MONITOR
+        Dim sPath As String = Application.StartupPath & AppMonitor
         Call ChangeAppSettings(sPath, "DEBUG", chkDebugAM.Checked)
         Call ChangeAppSettings(sPath, "CONSOLE", chkConsoleAM.Checked)
         Call ChangeAppSettings(sPath, "USE_LOGFILE", chkLogFileAM.Checked)
@@ -127,7 +127,7 @@ Public Class Form1
     ''' save changes to the application profilers config file
     ''' </summary>
     Sub SaveProfilerSettings()
-        Dim sPath As String = Application.StartupPath & APP_PROFILER
+        Dim sPath As String = Application.StartupPath & AppProfiler
         Call ChangeAppSettings(sPath, "DEBUG", chkDebug.Checked)
         Call ChangeAppSettings(sPath, "CONSOLE", chkConsole.Checked)
         Call ChangeAppSettings(sPath, "USE_LOGFILE", chkLogFile.Checked)
@@ -147,7 +147,7 @@ Public Class Form1
     ''' save changes to the Data Dumper config file
     ''' </summary>
     Sub SaveDataDumperSettings()
-        Dim sPath As String = Application.StartupPath & APP_DATADUMP
+        Dim sPath As String = Application.StartupPath & AppDatadump
         Call ChangeAppSettings(sPath, "DEBUG", chkDebugDD.Checked)
         Call ChangeAppSettings(sPath, "CONSOLE", chkConsoleDD.Checked)
         Call ChangeAppSettings(sPath, "USE_LOGFILE", chkLogFileDD.Checked)
@@ -164,9 +164,9 @@ Public Class Form1
     ''' Save the database information to both known config files
     ''' </summary>
     Sub SaveDatabase()
-        Call SaveDatabaseSettings(Application.StartupPath & APP_PROFILER)
-        Call SaveDatabaseSettings(Application.StartupPath & APP_MONITOR)
-        Call SaveDatabaseSettings(Application.StartupPath & APP_DATADUMP)
+        Call SaveDatabaseSettings(Application.StartupPath & AppProfiler)
+        Call SaveDatabaseSettings(Application.StartupPath & AppMonitor)
+        Call SaveDatabaseSettings(Application.StartupPath & AppDatadump)
     End Sub
     ''' <summary>
     ''' Generic sub to save the database information to the config file that is passed 
@@ -191,8 +191,8 @@ Public Class Form1
     ''' <summary>
     ''' Properly exit the application
     ''' </summary>
-    ''' <param name="ExitValue"></param>
-    Sub ExitApp(Optional ByVal ExitValue As Integer = 0)
+    ''' <param name="exitValue"></param>
+    Sub ExitApp(Optional ByVal exitValue As Integer = 0)
         Application.Exit()
         'Environment.Exit(ExitValue)
     End Sub
