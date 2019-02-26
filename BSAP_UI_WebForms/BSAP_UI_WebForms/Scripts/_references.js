b6793b63516864892f9667caf0dcf0a51c7510dd
Joe Mireles <joe.mireles@atlasied.com>
2019-02-26 07:37:01 -0500

    #25 Removing and Fixing vulnerabilities found by github

--- BSAP_UI_WebForms/BSAP_UI_WebForms/Scripts/_references.js
+++ BSAP_UI_WebForms/BSAP_UI_WebForms/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="modernizr-2.6.2.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
 /// <reference path="jquery-3.3.1.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
