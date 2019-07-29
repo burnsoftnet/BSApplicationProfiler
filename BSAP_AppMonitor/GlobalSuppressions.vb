15a77241b1e6d8fd93450a3140230bcc7566ef73
Joe Mireles <joe.mireles@burnsoft.net>
2019-07-29 17:09:27 -0400

    #26 Updated suppression file

---
+++ BSAP_AppMonitor/GlobalSuppressions.vb
@@ -0,0 +1,8 @@
+﻿
+' This file is used by Code Analysis to maintain SuppressMessage 
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given 
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:BSAP_AppMonitor.BurnSoft.BsDatabase.ConnExe(System.String)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:BSAP_AppMonitor.modMain.getSessionID~System.Int64")>
---
+++ BSAP_UI_WebForms/BSAP_UI_WebForms/GlobalSuppressions.vb
@@ -0,0 +1,9 @@
+﻿
+' This file is used by Code Analysis to maintain SuppressMessage 
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given 
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:BSAP_UI_WebForms.BurnSoft.BSDatabase.ConnEXE(System.String,System.Int64@,System.String)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:BSAP_UI_WebForms.RouteConfig.RegisterRoutes(System.Web.Routing.RouteCollection)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="<Pending>", Scope:="member", Target:="~M:BSAP_UI_WebForms.BurnSoft.BSAP.BSSessionDetailsStats.getSessionAverage(System.Int64,System.String@,System.String@,System.String@,System.String@,System.Int64@,System.String)")>
